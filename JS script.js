6fe9c45643b37428655467ced4e88a62ea94e17a
guilhermeyasmin-ai <guilherme.yasmin@escola.pr.gov.br>
2026-06-02 10:35:26 -0300

    Update and rename README.md to  JS script.js

---
+++ JS script.js
@@ -0,0 +1,15 @@
+//VERSÃO 1
+function funcao1() {
+var x = document.getElementById("il").value;
+var y document.getElementById("12").value;
+var zx * y;
+if (z > 100) {
+document.getElementById("res").style.color = "red";
+alert("Muito caro!");
+} else {
+document.getElementById("res").style.color = "green";
+document.getElementById("res").innerHTML = "Total: " + z;
+console.log("Calculado"):
+
+  document.getElementById("res").innerHTML = "Total: " + z;
+console.log("Calculado");
